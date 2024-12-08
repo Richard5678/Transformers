e6aa92f564cfbd50d7d537c6ef2ec14eefee92ed
richard <frqrichard@gmail.com>
2024-12-08 01:59:51 -0500

    redesigned file structure and implemented multihead attention and feed forward block

--- transformers/blocks.py
+++ transformers/blocks.py
@@ -1,40 +1,49 @@
 """Model Architectures
 
 Implementation of components of Transformer architecture:
-    - Decoder block
-    - Encoder block
+    - Multi-head attention
+    - Feed-forward network
 
 """
 
 import torch.nn as nn
 
-class DecoderBlock(nn.Module):
-    def __init__(self, embed_dim, num_heads, hidden_dim, dropout=0.1):
-        super(DecoderBlock, self).__init__()
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
         
-        self.multi_attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
+        self.multi_head_attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
         self.dropout1 = nn.Dropout(dropout)
         self.norm1 = nn.LayerNorm(embed_dim)
-        self.ff = nn.Sequential(
-            nn.Linear(embed_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, embed_dim),
-            nn.Dropout(dropout),
-        )
         
-        self.norm2 = nn.LayerNorm(embed_dim)
-
-    def forward(self, x, attn_mask=None):
-        attn_output, _ = self.multi_attn(x, x, x, attn_mask=attn_mask)
+    def forward(self, query, key, value, attn_mask=None):
+        attn_output, _ = self.multi_head_attn(query, key, value, attn_mask=attn_mask)
 
         # add & norm
-        x += self.dropout1(attn_output)
-        x = self.norm1(x)
-        
-        ff_out = self.ff(x)
-        
+        value += self.dropout1(attn_output)
+        value = self.norm1(value)
+
+        return value
+
+
+class FeedForwardNetwork(nn.Module):
+    def __init__(self, embed_dim, hidden_dim, dropout=0.1):
+        super(FeedForwardNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(embed_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(hidden_dim, embed_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.norm = nn.LayerNorm(embed_dim)
+    def forward(self, x):
+        ff_output = self.linear1(x)
+        ff_output = self.relu(ff_output)
+        ff_output = self.dropout(ff_output)
+        ff_output = self.linear2(ff_output)
+
         # add & norm
-        x += ff_out
-        x = self.norm2(x)
+        x += ff_output
+        x = self.norm(x)
 
         return x
