94944337b6786b226c62b03c9f4f223932ba714e
richard <frqrichard@gmail.com>
2024-12-08 23:26:35 -0500

    fixed naming updates

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from transformers.blocks import MultiHeadAttention, FeedForwardNetwork
+from transformers.blocks import MultiHeadAttentionBlock, FeedForwardNetworkBlock
 import torch
 
 
@@ -8,7 +8,7 @@ class TestMultiHeadAttention(unittest.TestCase):
         embed_dim = 10
         bat_size = 50
     
-        multi_head_attn = MultiHeadAttention(embed_dim=embed_dim, num_heads=5)
+        multi_head_attn = MultiHeadAttentionBlock(embed_dim=embed_dim, num_heads=5)
         input = torch.rand((bat_size, embed_dim))
         output = multi_head_attn(input, input, input)
 
@@ -20,7 +20,7 @@ class TestFeedForwardNetwork(unittest.TestCase):
         embed_dim = 10
         bat_size = 50
     
-        feed_forward_network = FeedForwardNetwork(embed_dim=embed_dim, hidden_dim=3)
+        feed_forward_network = FeedForwardNetworkBlock(embed_dim=embed_dim, hidden_dim=3)
         input = torch.rand((bat_size, embed_dim))
         output = feed_forward_network(input)
 
