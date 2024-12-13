9d60063aa60e3d2aae3f4cc512c8a6e350bd4122
richard <frqrichard@gmail.com>
2024-12-13 02:20:52 -0500

    finished sentiment analysis example

--- examples/sentiment_analysis.py
+++ examples/sentiment_analysis.py
@@ -1,29 +1,22 @@
-from datasets import load_dataset
-import pandas as pd
 from transformers import AutoTokenizer
 from mytransformers.models import TransformerEncoderOnly
+from sklearn.metrics import confusion_matrix
+from datasets import load_dataset
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
-import torch.nn as nn
 from torch.optim import Adam
-from sklearn.metrics import confusion_matrix
+import torch.nn as nn
 from tqdm import tqdm
-from torch.cuda.amp import GradScaler, autocast
+from sklearn.metrics import confusion_matrix
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
-
-def load_data():
-    dataset = load_dataset("imdb")
-
-    train_dataset = dataset["train"]
-    test_dataset = dataset["test"]
-    return train_dataset, test_dataset
 
 
 def tokenize_data(dataset, tokenizer):
     X = tokenizer(dataset["text"], padding=True, truncation=True, return_tensors="pt")
     Y = torch.tensor(dataset["label"])
+
     return X, Y
 
 
@@ -33,96 +26,104 @@ class CustomDataset(torch.utils.data.Dataset):
         self.labels = labels
 
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
+        return self.encodings["input_ids"][idx], self.labels[idx]
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.encodings["input_ids"])
 
 
-def train_model(model, train_loader, epochs=10):
+def train_model(model, train_loader, epoch=1):
     optimizer = Adam(model.parameters(), lr=1e-5)
     criterion = nn.CrossEntropyLoss()
-    scaler = GradScaler()
 
-    for i in range(epochs):
-        for X in tqdm(train_loader):
-            input_ids = X['input_ids'].to(device)
-            labels = X['labels'].to(device)
+    model.train()
 
+    for i in range(epoch):
+        for input_ids, labels in tqdm(train_loader):
             optimizer.zero_grad()
-            with autocast():
-                output = model(input_ids)
-                logits = output[:, 0, :]
-                loss = criterion(logits.view(-1, logits.shape[-1]), labels.view(-1))
-            
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
 
-        print(f"Epoch {i+1} loss: {loss.item()}")
+            # forward pass
+            input_ids, labels = input_ids.to(device), labels.to(device)
+            output = model(input_ids)  # (batch_size, seq_len, num_labels)
+
+            # loss calculation
+            logits = output[:, 0, :]  # (batch_size, num_labels)
+            loss = criterion(logits, labels)
+
+            # backward pass
+            loss.backward()
+            optimizer.step()
 
 
 def evaluate_model(model, test_loader):
     model.eval()
-    criterion = nn.CrossEntropyLoss()
 
     with torch.no_grad():
-        for X, Y in test_loader:
-            X, Y = X.to(device), Y.to(device)
-            output = model(X)
+        criterion = nn.CrossEntropyLoss()
+        average_accuracy = average_loss = 0
+        all_predictions = []
+        all_labels = []
 
-            # accuracy
-            accuracy = (output.argmax(dim=1) == Y).float().mean()
-            print(f"Test accuracy: {accuracy.item()}")
-
-            # confusion matrix
-            conf_matrix = confusion_matrix(
-                Y.cpu().numpy(), output.argmax(dim=1).cpu().numpy()
-            )
-            print(f"Confusion matrix: {conf_matrix}")
+        for input_ids, labels in tqdm(test_loader):
+            # forward pass
+            input_ids, labels = input_ids.to(device), labels.to(device)
+            output = model(input_ids)  # (batch_size, seq_length, num_labels)
 
             # loss calculation
-            loss = criterion(output, Y)
-            print(f"Test loss: {loss.item()}")
+            logits = output[:, 0, :]  # (batch_size, num_labels)
+            loss = criterion(logits, labels)
+            average_loss += loss.item()
+
+            predictions = logits.argmax(dim=1)
+            average_accuracy += (predictions == labels).float().mean()
+
+            all_predictions.append(predictions.cpu().numpy())
+            all_labels.append(labels.cpu().numpy())
+
+        average_loss /= len(test_loader)
+        average_accuracy /= len(test_loader)
+
+        print(f"average loss is: {average_loss}")
+        print(f"average accuracy is: {average_accuracy}")
+
+        matrix = confusion_matrix(
+            np.concatenate(all_labels), np.concatenate(all_predictions)
+        )
+
+        print(f"confusion matrix:\n {matrix}")
 
 
 def main():
     # load dataset
     dataset = load_dataset("imdb")
 
-    # initialize tokenizer
+    # tokenization
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    train_encodings, train_labels = tokenize_data(dataset["train"], tokenizer)
+    test_encodings, test_labels = tokenize_data(dataset["test"], tokenizer)
 
-    # tokenize datasets
-    train_dataset, test_dataset = dataset["train"], dataset["test"]
-    train_encodings, train_labels = tokenize_data(train_dataset, tokenizer)
-    test_encodings, test_labels = tokenize_data(test_dataset, tokenizer)
-
-    # create custom datasets
+    # dataset construction
     train_dataset = CustomDataset(train_encodings, train_labels)
     test_dataset = CustomDataset(test_encodings, test_labels)
 
-    # create dataloaders
     train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
     test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=4)
 
-    # create model
+    # model creation
     model = TransformerEncoderOnly(
         embed_dim=768,
-        num_heads=12,
+        num_heads=16,
         vocab_size=tokenizer.vocab_size,
         num_labels=2,
         max_seq_length=512,
-        num_layers=12,
+        num_layers=3,
         hidden_dim=768,
     ).to(device)
 
-    # train model
+    # model training
     train_model(model, train_loader)
 
-    # evaluate model
+    # model evaluation
     evaluate_model(model, test_loader)
 
 
