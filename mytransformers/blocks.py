280e05c038d5206f71fa5637e702aeb0e46508b6
richard <frqrichard@gmail.com>
2024-12-15 02:47:01 -0500

    supported padding mask inside multi head attention block

--- mytransformers/blocks.py
+++ mytransformers/blocks.py
@@ -12,13 +12,17 @@ import torch.nn as nn
 class MultiHeadAttentionBlock(nn.Module):
     def __init__(self, embed_dim, num_heads, dropout=0.1):
         super(MultiHeadAttentionBlock, self).__init__()
-        
-        self.multi_head_attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
+
+        self.multi_head_attn = nn.MultiheadAttention(
+            embed_dim, num_heads, dropout=dropout
+        )
         self.dropout1 = nn.Dropout(dropout)
         self.norm1 = nn.LayerNorm(embed_dim)
-        
-    def forward(self, query, key, value, attn_mask=None):
-        attn_output, _ = self.multi_head_attn(query, key, value, attn_mask=attn_mask)
+
+    def forward(self, query, key, value, attn_mask=None, key_padding_mask=None):
+        attn_output, _ = self.multi_head_attn(
+            query, key, value, attn_mask=attn_mask, key_padding_mask=key_padding_mask
+        )
 
         # add & norm
         value = value + self.dropout1(attn_output)
